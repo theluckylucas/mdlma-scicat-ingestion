48dbee6aa207d9386b9b6eb8c9b2b65403317fb2
Christian Lucas <lucaschr@max-hzgg002.desy.de>
2020-10-20 14:09:29 +0200

    Add Delete scripts

---
+++ DeleteP07.py
@@ -0,0 +1,17 @@
+"""
+This is the main file to call for deletion of P05 experiments.
+Run python DeleteP07.py -h for details about how to call.
+"""
+
+
+from ScicatTool.Sites.P07.Deletion import P07Deleter
+from ScicatTool.Utils.ArgumentsBeamline import P07ExperimentDeletionParser
+import datetime
+
+
+if __name__ == '__main__':
+    print('START', datetime.datetime.now())
+    parser = P07ExperimentDeletionParser()
+    args = parser.parse_args()
+    P07Deleter(args).delete_experiment()
+    print('END', datetime.datetime.now())
---
+++ _delete_all_models.py
@@ -0,0 +1,28 @@
+"""
+This is a script for deleting all attachments
+"""
+
+import requests
+import json
+
+
+model = "attachments"
+token = "dStUEq2Or7SQ1YXbPaEut2O4JTujroSxs1bYid7N5rOvWsL3nD55hbcfESCHN8Fh"
+
+
+HEADERS = {
+    'Content-Type': 'application/json',
+    'Accept':       'application/json'
+}
+URL = "https://scicat-mdlma.desy.de/api/v3/{}/{}?access_token={}"
+ID = "id"
+
+
+if __name__ == '__main__':
+    resp = requests.get(URL.format(model, "findOne", token), headers=HEADERS, data={})
+    while resp.status_code == 200:
+        dic = json.loads(resp.text)
+        aid = dic[ID]
+        print(aid, requests.delete(URL.format(model, aid, token), headers=HEADERS, data={}))
+        resp = requests.get(URL.format(model, "findOne", token), headers=HEADERS, data={})
+
